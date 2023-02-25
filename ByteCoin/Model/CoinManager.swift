2dbb30672d497e60601ccfb3fdc60b72db8a94eb
Janibek <jano.vr@mail.ru>
2023-02-25 09:34:39 +0400

    Initial Commit

---
+++ ByteCoin/Model/CoinManager.swift
@@ -0,0 +1,17 @@
+//
+//  CoinManager.swift
+//  ByteCoin
+//
+//  Created by Janibek Voskanyan on 25.02.23.
+//
+
+import Foundation
+
+struct CoinManager {
+    
+    let baseURL = "https://rest.coinapi.io/v1/exchangerate/BTC"
+    let apiKey = "C6C03B48-4519-4769-B019-1824227B0D26"
+    
+    let currencyArray = [
+        "AUD","BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]
+}
